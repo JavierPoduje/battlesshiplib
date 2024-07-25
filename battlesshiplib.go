af3f750b5521b923b76ee92a13ba453af1eb1ef1
caxco93 <caxco93@gmail.com>
2024-07-25 18:26:32 -0500

    Add encode decode with redis

--- battlesshiplib.go
+++ battlesshiplib.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"encoding/gob"
 	"fmt"
+	"time"
 
 	"github.com/go-redis/redis/v8"
 )
@@ -32,12 +33,13 @@ func Connect() *redis.Client {
 	return rdb
 }
 
-func (r Redis) Set(key string, value any) {
-	_, err := r.rdb.Set(r.ctx, key, value, 0).Result()
+func (r Redis) Set(key string, value any, expiration time.Duration) error {
+	_, err := r.rdb.Set(r.ctx, key, value, expiration).Result()
 	if err != nil {
 		fmt.Println("Can't set key:", err)
-		return
+		return err
 	}
+	return nil
 }
 
 func (r Redis) Ping() {
@@ -49,7 +51,7 @@ func (r Redis) Ping() {
 	fmt.Println("Redis client started:", pong)
 }
 
-func (r Redis) Get(key string) any {
+func (r Redis) Get(key string) (string, error) {
 	val, err := r.rdb.Get(r.ctx, key).Result()
 	if err != nil {
 		if err == redis.Nil {
@@ -57,9 +59,9 @@ func (r Redis) Get(key string) any {
 		} else {
 			fmt.Println("No se pudo obtener la clave:", err)
 		}
-		return nil
+		return val, err
 	}
-	return val
+	return val, nil
 }
 
 func (r Redis) Del(key string, value string) {
--- battlesshiplib_test.go
+++ battlesshiplib_test.go
@@ -3,20 +3,21 @@ package battlesshiplib
 import (
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestBattlesshiplib_Connect(t *testing.T) {
 	r := NewRedis()
 	r.Ping()
-	r.Set("yo", "meo")
-	value := r.Get("yo")
+	r.Set("yo", "meo", 1)
+	value, _ := r.Get("yo")
 	if value != "meo" {
 		t.Errorf("Expected value: meo, got: %v", value)
 	}
 	defer r.rdb.Close()
 }
 
-func TestBattlesshiplib_SaveWithEncoding(t *testing.T) {
+func TestBattlesshiplib_EncodeDecode(t *testing.T) {
 	coordinate := Coordinate{X: 1, Y: 2}
 
 	// Encode to []byte
@@ -33,6 +34,38 @@ func TestBattlesshiplib_SaveWithEncoding(t *testing.T) {
 	}
 
 	if decodedCoordinate.X != 1 || decodedCoordinate.Y != 2 {
+		t.Errorf("Expected value: meo, got: %v", decodedCoordinate)
+	}
+
+	fmt.Printf("Decoded coordinate: %+v\n", decodedCoordinate)
+}
+
+func TestBattlesshiplib_SaveWithEncoding(t *testing.T) {
+	r := NewRedis()
+
+	coordinate := Coordinate{X: 5, Y: 3}
+
+	// Encode to []byte
+	data, err := Encode(coordinate)
+	if err != nil {
+		fmt.Println("Error encoding:", err)
+		return
+	}
+
+	r.Set("coordinate", string(data), time.Second*1)
+
+	retrievedData, err := r.Get("coordinate")
+
+	// Cast retrievedData to []byte
+	decodedData := []byte(retrievedData)
+
+	// Simulate reading `data` from Redis and decoding it
+	decodedCoordinate, err := Decode[Coordinate](decodedData)
+	if err != nil {
+		t.Errorf("Expected value to be a Coordinate, got: %v", decodedCoordinate)
+	}
+
+	if decodedCoordinate.X != 5 || decodedCoordinate.Y != 3 {
 		t.Errorf("Expected value: meo, got: %v", decodedCoordinate)
 	}
 
